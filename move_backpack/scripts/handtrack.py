fe7bda70b8c224b017fba6ff9559ecddd3716330
ludicao <ludicao0427@gmail.com>
2019-07-08 17:28:57 -0400

    temp save

--- move_backpack/scripts/handtrack.py
+++ move_backpack/scripts/handtrack.py
@@ -9,39 +9,38 @@ import rospy
 import moveit_commander
 from trac_ik_python.trac_ik import IK
 import hebi
+import subprocess
+import sys
+import os
+from pp import Poses
+from std_msgs.msg import String
+from std_msgs.msg import Float32MultiArray
 
 class PoseObject(object):
     def __init__(self):
         super(PoseObject, self).__init__()
         self.stop = False  # True when hits obstacles
-        self.initial_pose = True  # In plan path mode
-        self.counter = 0
-
-        rospy.Subscriber("/aruco_single/pose", PoseStamped, self.fb)
-        rospy.Subscriber("/joint_states", JointState, self.js_callback)
+        self.plan_path= True  # In plan path mode	
+	self.last = None
+	self.check = False
+        rospy.Subscriber("/aruco_single/pose", PoseStamped, self.fb, queue_size=1)
+        rospy.Subscriber("/move_group/fake_controller_joint_states", JointState, self.js_callback, queue_size=1)
         rospy.Subscriber("/hebiros/right_arm/feedback/joint_state", JointState, self.limit, queue_size=1)
+        rospy.Subscriber("plan_status", String, self.plan_status, queue_size=1)
+	rospy.Subscriber("joint_states", JointState, self.keep_running, queue_size = 1)
 
         self.hebi = hebi.Lookup().get_group_from_names(['*'], ['base_temp', 'X-00476', 'elbow_temp'])
 
-        self.pub = rospy.Publisher('joint_states', JointState, queue_size=1)
-        self.pubMark = rospy.Publisher("visualization_marker", Marker, queue_size=1) 
+        self.pub = rospy.Publisher('/move_group/fake_controller_joint_states', JointState, queue_size=1)
+	self.pubBool = rospy.Publisher('pose', Float32MultiArray, queue_size = 1)
 
         self.js = JointState()
-
         urdf_str = rospy.get_param('/robot_description')
         self.ik_solver = IK("world", "end_link/INPUT_INTERFACE",  urdf_string=urdf_str)
 
-        self.group = moveit_commander.MoveGroupCommander("right_arm")
-    
-        # Feedback data of joint angles from hebi for recalculation of IK
-        self.x, self.y, self.z = 0, 0, 0
-        '''
-        self.alpha = .25    # For filtering
-        self.filter_pose = np.array([0.0, 0.0, 1.0])
-        '''
         # Current pose angles when arm hits object
         self.joint_pose = [0, 0, 0]
-
+	'''
         # Marker for visualization of apriltag in Rviz
         self.robotMarker = Marker()
         self.robotMarker.header.frame_id = "world"
@@ -67,30 +66,36 @@ class PoseObject(object):
         self.robotMarker.color.a = 1.0
 
         self.robotMarker.lifetime = rospy.Duration(0)
-
+	'''
         print("start")
 
     # Record current joint angles of arm when it hits objects
     def limit(self, data):
-        if self.threshold(data.position, data.effort):
-            self.stop = True
-        if self.stop:
+        self.js = data
+        if self.threshold(data.position, data.effort) or self.stop:
+	    if self.threshold(data.position, data.effort):
+	    	self.joint_pose = data.position
+                self.first = True	
+
+	    print("in limit loop", self.joint_pose)
             group_command = hebi.GroupCommand(3)
-            group_command.position = data.position
+            group_command.position = self.joint_pose
             self.hebi.send_command(group_command)
-            self.js.position = data.position 
-            self.counter += 1
-            self.pub.publish(self.js)
-            # Ensures that joint state messages have fully arrived 
-            if self.counter >= 5:
-                self.initial_pose = True
-                self.counter = 0
+	    
+            if self.first:
+		self.js.position = self.joint_pose
+	        self.pub.publish(self.js)
+		while self.pub.get_num_connections() == 0:
+                    rospy.sleep(.1)
+		self.plan_path = True
+                self.stop = True
+		self.first = False
 
 
     # Execute planned path through joint state messages
     def js_callback(self, data):
-        self.js = data
-        if self.initial_pose and not self.stop:  
+        if self.plan_path and not self.stop:  
+	    print("in js_callback loop")
             group_command = hebi.GroupCommand(3)
             positions = np.array([data.position[0], data.position[1], data.position[2]])
             #self.filter_pose = self.alpha*self.filter_pose+(1-self.alpha)*positions
@@ -98,88 +103,96 @@ class PoseObject(object):
             group_command.position = positions
             self.hebi.send_command(group_command)
 
+    def plan_status(self, data):
+	print("in listener2", self.plan_path, data)
+	if self.plan_path and not self.stop:
+            if data.data == "yes":
+		self.stop = False 
+	    if data.data == "yes2":
+		self.plan_path = False
+    
+
+
     # Subscriber callback to april tag position
     def fb(self, data):
-        jspose = self.js.position
-        self.x = data.pose.position.x
-        self.y = data.pose.position.y
-        self.z = data.pose.position.z
+        x = data.pose.position.x
+        y = data.pose.position.y
+        z = data.pose.position.z
 
         xyz_hebi = [0, 0, 0]
-        xyz_hebi[0] = self.z-.3
-        xyz_hebi[1] = -self.x + .15
-        xyz_hebi[2] = -self.y + .6
+        xyz_hebi[0] = z-.3
+        xyz_hebi[1] = -x + .15
+        xyz_hebi[2] = -y + .6
 
-        #self.old_pose = (1-self.alpha)*xyz_hebi + alpha*self.old_pose
+	self.xyz_hebi = xyz_hebi
     
-        self.robotMarker.pose.position.x = xyz_hebi[0]
-        self.robotMarker.pose.position.y = xyz_hebi[1]
-        self.robotMarker.pose.position.z = xyz_hebi[2]  
+        #self.robotMarker.pose.position.x = xyz_hebi[0]
+        #self.robotMarker.pose.position.y = xyz_hebi[1]
+        #self.robotMarker.pose.position.z = xyz_hebi[2]
+	if not self.check:
+	    self.check = True 
 
-        self.pubMark.publish(self.robotMarker)
 
-        #xyz_hebi = self.old_pose
 
-        # Path plan with collision checking to initial pose
-        if self.initial_pose:
-            pose_goal = geometry_msgs.msg.Pose()
-            pose_goal.orientation.w = 1.0
-            pose_goal.position.x = xyz_hebi[0]
-            pose_goal.position.y = xyz_hebi[1]
-            pose_goal.position.z = xyz_hebi[2]
+    def keep_running(self, data):
+        jspose = self.js.position
+        if self.check:
+            xyz_hebi = self.xyz_hebi
+            # Path plan with collision checking to initial pose
+            if self.plan_path and self.stop:
+	        print("in if self.plan_path in fb")
 
-            self.group.set_pose_target(pose_goal)
-            self.group.set_goal_orientation_tolerance(3)
-            self.group.set_goal_joint_tolerance(.05)
-            
-            plan = self.group.plan()
+	        f = Float32MultiArray()
+	        f.data = xyz_hebi
+	        self.pubBool.publish(f)
+	        while self.pubBool.get_num_connections() == 0:
+		    rospy.sleep(.1)
 
-            if plan[0]:
-                self.stop = False
-                self.group.execute(plan[1], wait=False)
-                self.group.stop()
-                self.initial_pose = False
-        
-
-        # Handtrack without collision checking afterwards
-        elif not self.initial_pose and not self.stop:
-            position_approx = [jspose[0], jspose[1], jspose[2]]
-            positions = self.ik_solver.get_ik(position_approx, xyz_hebi[0], xyz_hebi[1], xyz_hebi[2], 0.0, 0.0, 0.0, 1.0)
-            if positions == None:
-                #print("no joint states found")
-                positions = position_approx
-
-            group_command = hebi.GroupCommand(3)
-            group_command.position = positions
-            self.hebi.send_command(group_command)
-            joints = (positions[0], positions[1], positions[2])
-            self.js.position = joints
-            self.pub.publish(self.js)
+            # Handtrack without collision checking afterwards
+            if not self.plan_path and not self.stop:
+	        print("in handtracking")
+                position_approx = [jspose[0], jspose[1], jspose[2]]
+                positions = self.ik_solver.get_ik(position_approx, xyz_hebi[0], xyz_hebi[1], xyz_hebi[2], 0.0, 0.0, 0.0, 1.0, .01, .01, .01)
+	        if self.last == None:
+		    self.last = position_approx
+                if positions == None:
+                    print("no joint states found")
+                    positions = self.last
+                group_command = hebi.GroupCommand(3)
+                group_command.position = positions
+                self.last = positions
+                self.hebi.send_command(group_command)
 
 
     # Joint limits based on position
     # Temporary method right now once gravity compensation works
     def threshold(self, joints, efforts):
         if joints[0] > -1.57 and joints[0] < 1.57:
-            if efforts[0] > 4 or efforts[0] < -3:
+            if efforts[0] > 10 or efforts[0] < -7:
+	        print("1111111111111111111111111111111111111")
                 return True
         else:
-            if efforts[0] < -5 or efforts[0] > 3:
-            return True
+            if efforts[0] < -8 or efforts[0] > 5:
+		print("22222222222222222222222222222222222222222")
+            	return True
 
         if joints[1] > 0 and joints[1] < 3.14:
-            if efforts[1] < -7 or efforts[1] > 5:
-            return True
+            if efforts[1] < -8 or efforts[1] > 6:
+		print("33333333333333333333333333333333333333333")
+            	return True
         else:                               
-            if efforts[1] > 7 or efforts[1] < -8:
+            if efforts[1] > 8 or efforts[1] < -9:
+		print("44444444444444444444444444444444444444444")
                 return True
 
         if joints[2] > 0 and joints[2] < 3.14:
-            if efforts[2] > 5 or efforts[2] < -4:
+            if efforts[2] > 6 or efforts[2] < -4:
+		print("55555555555555555555555555555555555555555")
                 return True
         else:
-            if efforts[2] < -4 or efforts[2] > 5:
-            return True
+            if efforts[2] < -6 or efforts[2] > 5:
+		print("666666666666666666666666666666666666666666")
+            	return True
  
         return False
 
